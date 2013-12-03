06eb2075ffa4a40cb4a375643c7c783c3ca286be
Jon Cram <jon@webignition.net>
2013-12-03 17:59:39 +0000

    Barebones does nothing with tests

---
+++ src/webignition/HtmlFragmentExtractor/HtmlFragmentExtractor.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace webignition\HtmlFragmentExtractor;
+
+class HtmlFragmentExtractor {
+    
+}
---
+++ tests/webignition/Tests/HtmlFragmentExtractor/TestTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace webignition\Tests\HtmlFragmentExtractor;
+
+use webignition\HtmlFragmentExtractor\HtmlFragmentExtractor;
+
+class TestTest extends BaseTest { 
+    
+    public function testTest() {
+        $extractor = new HtmlFragmentExtractor();
+    }
+    
+}
